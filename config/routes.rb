21bfd389142f38ee33e9ed430c7264c425514cb1
Joe Ganobsik <jganobsik@gmail.com>
2019-04-29 11:24:36 -0400

    create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/signup' => 'users#new'
+  get '/login'  => 'sessions#new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
