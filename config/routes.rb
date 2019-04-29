5b2ecb5976a06013ad702397f0170c9388fa409e
Joe Ganobsik <jganobsik@gmail.com>
2019-04-29 11:25:26 -0400

    route login through sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/signup' => 'users#new'
   get '/login'  => 'sessions#new'
+  post '/login' => 'sessions#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
