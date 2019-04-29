e88c580a50668f87a73be2cbf3625062b685e08f
Joe Ganobsik <jganobsik@gmail.com>
2019-04-29 19:38:42 -0400

    finish session creation/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,21 @@
 class SessionsController < ApplicationController
     def new
+        @user = User.new
         render :login
     end
 
     def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to car_path
+        else
+            render :new
+        end
     end
 
     def destroy
+        session.clear 
+        redirect_to '/'
     end
 end
