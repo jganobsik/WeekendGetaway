25e99f885641752996589bab2c2c08ef74eb99b5
Joe Ganobsik <jganobsik@gmail.com>
2019-04-30 16:43:49 -0400

    renamed to WeekendGetaway

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id 
             redirect_to car_path
         else
-            render :new
+            redirect_to 'login'
         end
     end
 
