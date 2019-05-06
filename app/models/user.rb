2d2d12682f2d5070894956eca0ddcfa64e3a9f87
Joe Ganobsik <jganobsik@gmail.com>
2019-05-06 10:33:37 -0400

    create has_many through associations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: true
-    has_many :getaways
-    
+    has_many :getaways,
+    has_many :trips, through: :getaways
+    has_many :accommodations, through: :getaways
+    has_many :excursions, through: :getaways
+
 end
