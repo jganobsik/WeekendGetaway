d8e65d386ebfb4bb7e8568731b1680dac3be293b
Joe Ganobsik <jganobsik@gmail.com>
2019-05-07 11:24:18 -0400

    add getaway new and create route

--- app/controllers/getaways_controller.rb
+++ app/controllers/getaways_controller.rb
@@ -1,5 +1,23 @@
 class GetawaysController < ApplicationController
 
+    def new
+
+        if params[:travel_agency_id] && travel_agency = TravelAgency.find_by_id(params[:travel_agency_id])
+          @getaway = travel_agency.getaways.build
+        else
+          @getaway = Getaway.new
+        end
+      end
+    
+    
+      def create
+        @getaway = current_user.getaways.build(getaway_params)
+        if @getaway.save
+          redirect_to getaway_path(@getaway)
+        else
+          render :new
+        end
+      end
 
 end
 
