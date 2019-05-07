721bcb7a65f665c2a89a3f97f3ad96c06993f2e3
Joe Ganobsik <jganobsik@gmail.com>
2019-05-07 10:50:49 -0400

    make agency optional for getaways

--- app/models/getaway.rb
+++ app/models/getaway.rb
@@ -1,6 +1,6 @@
 class Getaway < ApplicationRecord
     belongs_to :user,
-    belongs_to :travel_agency, 
+    belongs_to :travel_agency, optional: true
     has_many :accomodations, 
     has_many :excursions,
     has_many :trips,
