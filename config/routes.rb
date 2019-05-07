6392a0627365d1b2792ef8dcff51cf3c2f34ba5e
Joe Ganobsik <jganobsik@gmail.com>
2019-05-07 10:28:57 -0400

    add sessions helpers and getaway route

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   resources :users
+  resources :getaways
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
