9130aa538460360e5a3714fb1fd297abea8a6cd6
Joe Ganobsik <jganobsik@gmail.com>
2019-05-31 14:41:41 -0400

    add agency create params

--- app/controllers/travel_agencies_controller.rb
+++ app/controllers/travel_agencies_controller.rb
@@ -1,2 +1,17 @@
 class TravelAgenciesController < ApplicationController
+
+    def new
+        @agency = TravelAgency.new
+    end
+    
+    def create 
+        @agency = TravelAgency.new(travel_agency_params)
+    
+        if @agency.save
+         
+            redirect_to travel_agency_path
+        else 
+            render :new
+        end
+    end
 end
