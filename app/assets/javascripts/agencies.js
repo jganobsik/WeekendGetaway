a449ae72d96ceba369cda384193bf02d9a9d2860
Joe Ganobsik <jganobsik@gmail.com>
2019-07-07 00:01:49 -0400

    finish show

--- app/assets/javascripts/agencies.js
+++ app/assets/javascripts/agencies.js
@@ -20,11 +20,22 @@ $('.agencies').on('click', (e) =>{
     })  
 
 })
-$(".show_link").on('click', (e) =>
-e.preventDefault()
-$('#app-container').html('')
-)
-}
+    $(document).on('click', ".show_link", (e) =>{
+        e.preventDefault()
+        history.pushState(null, null, "travel_agencies/${id}")
+        let id = $(this).attr('data-id')
+        fetch(`/travel_agencies/${id}.json`)
+         .then(r => r.json())
+         .then(resp => {
+        $('#app-container').html('')
+            let newAgency = new Agency(resp)
+            let agencyHtml = newAgency.formatShow()
+            $('#app-container').append(agencyHtml)
+    })  
+    })
+    }
+
+
 
 function Agency(agency) {
     this.id = agency.id
@@ -34,7 +45,14 @@ function Agency(agency) {
 
 Agency.prototype.formatIndex = function(){
     let agencyHtml = `
-    <a href="/travel_agencies/${this.id}" class="show_link"></a><h1>${this.name}</h1></a>
+    <a href="/travel_agencies/${this.id}" class="show_link" data-id="${this.id}><h1>${this.name}</h1></a>
+    `
+    return agencyHtml
+}
+
+Agency.prototype.formatShow = function(){
+    let agencyHtml = `
+    <h1>${this.name}</h1>
     `
     return agencyHtml
 }
