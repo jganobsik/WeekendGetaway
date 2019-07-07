d9fa1c134a4b673b6a2c4f9912ce6247f7a24441
Joe Ganobsik <jganobsik@gmail.com>
2019-07-06 22:47:09 -0400

    make fetch request to route

--- app/assets/javascripts/agencies.js
+++ app/assets/javascripts/agencies.js
@@ -6,7 +6,10 @@ handleClick()
 const handleClick = () => {
 $('.agencies').on('click', (e) =>{
     e.preventDefault()
-    
+    fetch('/travel_agencies.json')
+    .then(r => r.json())
+    .then(resp => console.log(resp))
+
 })
 
 }
